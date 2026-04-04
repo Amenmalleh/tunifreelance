f14fe57578648e12c2ea44c5cb5590deb6704d5c
Ganed Chaima <ganedchayma29@gmail.com>
2026-04-04 22:36:15 +0100

    Page d'acceuil et design

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {}
